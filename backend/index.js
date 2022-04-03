e794e5aeeb614bee483f1368d1da45540b10659a
adidiac <diacadi@gmail.com>
2022-04-03 03:18:14 +0300

    express

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.DB_CONNECTION, () => {
 // Middlewares
 app.use(express.json({limit: '50mb'}));
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Route Middlewares
 app.use('/', homeRoute);
 app.use('/cameraRoute', cameraRoute);
